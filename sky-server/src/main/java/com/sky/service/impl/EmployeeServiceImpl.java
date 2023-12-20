324c2edbfacad398f22c2c729f45a6e5d3769af2
yexca <yexcano@gmail.com>
2023-12-20 16:47:55 +0800

    新增员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,7 +45,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 对前端密码进行MD5处理
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -54,4 +62,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+    /*
+    * 新增员工
+    * @Param employeeDTO
+    * */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee e = new Employee();
+        // 对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO,e);
+        // 设置账号状态，默认正常
+        e.setStatus(StatusConstant.ENABLE);
+        // 设置密码
+        e.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        // 创建时间与修改时间
+        e.setCreateTime(LocalDateTime.now());
+        e.setUpdateTime(LocalDateTime.now());
+        // 设置当前创建人ID与修改人ID
+        e.setCreateUser(BaseContext.getCurrentId());
+        e.setUpdateUser(BaseContext.getCurrentId());
+        // 持久层插入数据
+        employeeMapper.insert(e);
+    }
 }
