b18fdd710c1cb052a935cc64edb4535322c22ea6
yexca <yexcano@gmail.com>
2024-01-22 15:18:55 +0800

    删除菜品

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -3,9 +3,13 @@ package com.sky.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface SetmealMapper {
 
     @Select("select count(id) from setmeal where category_id = #{id}")
     Integer countByCategoryId(Long id);
+
+    List<Long> getSetmealIdsByDishIds(List<Long> ids);
 }
